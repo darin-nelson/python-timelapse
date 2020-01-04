5b6cd527a330de9a4d3dd5badf828cf005ed5319
darin-nelson <33072129+darin-nelson@users.noreply.github.com>
2020-01-03 21:17:10 -0800

    Initial versions for the project.

---
+++ calc_sunrise_sunset.py
@@ -0,0 +1,80 @@
+import datetime
+import time
+from math import sin, asin, acos, cos, tan, radians, degrees
+import pytz
+from datetime import datetime, timedelta
+
+def is_dst():
+    n = datetime.now()
+    dt = time.mktime((n.year, n.month, n.day, 12, 0, 0, -1, -1, -1))
+    return time.localtime(dt).tm_isdst
+   
+def convert_from_utc(julian_date):
+	unixTime = (julian_date - 2440587.5) * 86400
+	unixConvertDate = datetime(1970, 1, 1, 0, 0, 0, 0, tzinfo=pytz.utc)
+	return (unixConvertDate + timedelta(seconds=unixTime))
+	
+	
+# Calculations inspired by the wiki page:
+# https://en.wikipedia.org/wiki/Sunrise_equation
+def calc_runrise_sunset(date, longitude, latitude, timeZone):
+	debug = False
+	# M = Mean anomoly
+	# C = Equation of center
+	# lambda = elicptical longitude of the sun = eliptical_longitude_sun
+	# delta = declination of the sun 
+
+	# Compute julian date
+	unix_epoch = datetime(2000, 1, 1, 0, 0, 0, 0, tzinfo=pytz.utc)
+	date_utc = date.replace(tzinfo=pytz.utc)
+	days_since = (date_utc - unix_epoch)
+	julian_date = days_since.days + 2451545
+
+    # Compute julian cycle
+	julian_cycle = (julian_date - 2451545 - 0.0009) - (longitude / 360)
+	n = round(julian_cycle)
+	julian_solar_noon = 2451545 + 0.0009 + (longitude / 360) + n
+	M = (357.5291 + 0.98560028 * (julian_solar_noon - 2451545)) % 360
+	C = (1.9148 * sin(radians(M))) + (0.0200 * sin(2 * radians(M))) + (0.0003 * sin(3 * radians(M)))
+	eliptical_longitude_sun = (M + 102.9372 + C + 180) % 360
+	julian_date_solar_noon = julian_solar_noon + (0.0053 * sin(radians(M))) - \
+	                               (0.0069 * sin(2 * radians(eliptical_longitude_sun)))
+    # Compute declination of the sun
+	delta = degrees(asin(sin(radians(eliptical_longitude_sun)) * sin(radians(23.44))))
+	H = degrees(acos((sin(radians(-0.83)) - sin(radians(latitude)) * sin(radians(delta))) / \
+	                 (cos(radians(latitude)) * cos(radians(delta)))))
+    # Compute solar noon
+	julian_solar_noon2 = 2451545 + 0.0009 + ((H + longitude) / 360) + n
+	
+	julian_solar_noon_datetime = convert_from_utc(julian_date_solar_noon)
+    # Now compute sunrise and sunset (in julian format)
+	julian_sunset = julian_solar_noon2 + (0.0053 * sin(radians(M))) - (0.0069) * sin(2 * radians(eliptical_longitude_sun))
+	julian_date_sunrise = julian_date_solar_noon - (julian_sunset - julian_date_solar_noon)
+    # Now convert to UTC
+	sunrise_datetime = convert_from_utc(julian_date_sunrise).astimezone(timeZone)
+	sunset_datetime = convert_from_utc(julian_sunset).astimezone(timeZone)
+
+	if (debug == True):
+		print("julian date: %s" % julian_date)
+		print("n*: ", julian_cycle)
+		print("n: ", n)
+		print("julian_solar_noon:  ", julian_solar_noon)
+		print("Mean anomoly: ", M)
+		print("Equation of center: ", C)
+		print("eliptical longitude (lambda): ", eliptical_longitude_sun)
+		print("julian_date_solar_noon: ", julian_date_solar_noon)
+		print("delta (sun's declination): ", delta)
+		print("H (hour angle): ", H)
+		print ("julian_solar_noon2 (julian_solar_noon using H): ", julian_solar_noon2)
+		print("date time transit: ", julian_solar_noon_datetime)
+		print("date time transit tz: ", julian_solar_noon_datetime.astimezone(timeZone))
+		print("IS DST: ", is_dst())
+		print("julian_date_sunrise: ", julian_date_sunrise, " >>> ", sunrise_datetime)
+		print("julian_sunset: ", julian_sunset, " >>> ", sunset_datetime)
+	
+	return (sunrise_datetime, sunset_datetime)
+
+
+
+
+
---
+++ take_timelapse.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+from calc_sunrise_sunset import calc_runrise_sunset
+from datetime import datetime, timedelta
+import os
+from picamera import PiCamera
+import pytz
+import shlex
+import shutil
+import subprocess
+from time import sleep
+
+# This script is called using CRON to start the image capture each day
+# e.g.  /some/path/to/time lapse script/take_timelapse.py
+
+# To use this script, replace all values in angle brackets '<>' with appropriate
+# values for your application.
+ln = 40.71 # Specify longitude for calculation
+lat = 74.0 # Specify latitude for calculation
+delay = 30 # seconds
+tz = pytz.timezone('<TIME ZONE FOR CALCULATION>')
+
+date_time_now = datetime.now(tz=tz)
+path = "<LOCATION TO IMAGES DIR>/%s" % date_time_now.strftime('%d-%b-%Y')
+os.mkdir(path)
+os.chdir(path)
+
+# Calculate time values
+(sunrise, sunset) = calc_runrise_sunset(date_time_now, ln, lat, tz)
+# Time delta helps create start/stop time no actually sunrise/sunset
+time_offset = timedelta(hours=0, minutes=40)
+start_time = sunrise - time_offset 
+stop_time = sunset + time_offset
+sleep_time = start_time - date_time_now
+
+# Handles the case where the script was started manually
+if sleep_time.total_seconds() < 0:
+    sleep_time = timedelta()
+
+# If the script is started before sunrise, sleep until then
+sleep(sleep_time.total_seconds())
+date_time_now = datetime.now(tz=tz)
+
+if date_time_now >= start_time:
+    camera = PiCamera()
+    # Wait for the automatic gain control to settle
+    # Now fix the values
+    camera.awb_mode = 'auto'
+    sleep(2)
+    camera.start_preview()
+    sleep(2)
+    camera.stop_preview()
+
+    for filename in camera.capture_continuous('img-{counter:04d}.jpg'):
+        sleep(delay) # wait in seconds 
+        curr_time = datetime.now(tz=tz)
+
+        if curr_time >= stop_time:
+            break
+    
+    mp4_file = "%s.mp4" % curr_time.strftime("%d-%b-%Y")
+    # ffmpeg command to create an mp4 from all the images
+    cmd = "ffmpeg -r 25 -i img-%4d.jpg -vcodec libx264 -crf 25 -pix_fmt yuv420p " + mp4_file
+    subprocess.call(shlex.split(cmd))
+    # Convert pics to MP4 
+    # Clean up all pics from current day
+    month = curr_time.strftime("%B")
+    cmd = "python <PATH TO UPLOAD SCRIPT>/upload_video.py  --file <PATH TO GENERATED MP4> --title \"Appropriate video title\" --description \"Appropriate video description\" --privacyStatus public --keywords \"Appropriate keywords\""
+    
+    os.chdir("<CHANGE DIR TO upload_video.py LOCATION>")
+    output = subprocess.check_output(shlex.split(cmd))
+    result = output.find("successfully")
+    
+    # If the upload was successful, remove the directory
+    # that was created
+    if (result > 0):
+        shutil.rmtree(path)
